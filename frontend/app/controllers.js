2946694a95774cf97332d28272c270b1627ff59c
Victor Rocha <victor@rochapps.com>
2015-02-16 22:12:22 -0500

    Check for small board winning position

--- frontend/app/controllers.js
+++ frontend/app/controllers.js
@@ -4,54 +4,90 @@ angular.module('TicTacToe.controllers', [])
     }])
     .controller('VersusModeCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
         $rootScope.gameState = [
-            [
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}]
-            ],
-            [
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}]
-            ],
-            [
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}]
-            ],
-            [
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}]
-            ],
-            [
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}]
-            ],
-            [
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}]
-            ],
-            [
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}]
-            ],
-            [
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}]
-            ],
-            [
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}],
-                [{state: null}, {state: null}, {state: null}]
-            ],
+            {
+                status: 'available',
+                winner: null,
+                state: [
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}]
+                ]
+            },
+            {
+                status: 'available',
+                winner: null,
+                state: [
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}]
+                ]
+            },
+            {
+                status: 'available',
+                winner: null,
+                state: [
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}]
+                ]
+            },
+            {
+                status: 'available',
+                winner: null,
+                state: [
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}]
+                ]
+            },
+            {
+                status: 'available',
+                winner: null,
+                state: [
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}]
+                ]
+            },
+            {
+                status: 'available',
+                winner: null,
+                state: [
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}]
+                ]
+            },
+            {
+                status: 'available',
+                winner: null,
+                state: [
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}]
+                ]
+            },
+            {
+                status: 'available',
+                winner: null,
+                state: [
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}]
+                ]
+            },
+            {
+                status: 'available',
+                winner: null,
+                state: [
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}],
+                    [{state: null}, {state: null}, {state: null}]
+                ]
+            },
         ];
-        $rootScope.currentPlayer = 0;
-        $rootScope.currentBoard = 0;
+
+        $rootScope.currentPlayer = 1;
 
         $scope.$watch("gameState", function(newValue, oldValue) {
             // check if the current position is a winning position
--- frontend/app/directives.js
+++ frontend/app/directives.js
@@ -1,23 +1,68 @@
 angular.module('TicTacToe.directives', [])
     .directive('board', ['$rootScope', function($rootScope) {
         var linker = function(scope, element, attrs) {
-            var boardId = scope['$index'],
-                isBusy = false;
-            scope.boardId = boardId;
-            console.log(boardId);
+            var isBusy = false;
             scope.updateState = function(square, rowIndex, index) {
-                if ((square.state == null) && (boardId == $rootScope.currentBoard) && !isBusy) {
+                if ((square.state == null) && (scope.boardState.status == 'available') && !isBusy) {
                     // only squares that have not been played can be played.
-                    isBusy = true;
                     square.state = $rootScope.currentPlayer;
-                    $rootScope.currentPlayer = ($rootScope.currentPlayer == 0) ? 1 : 0;
-                    $rootScope.currentBoard = (rowIndex * 3) + index;
-                    scope.isCurrentBoard = false;
-                    isBusy = false;
+                    checkPosition(scope.boardState);
+                    updateBoardStatus((rowIndex * 3) + index);
                 } else {
                     console.log("this board can't be played");
                 }
             };
+
+            var winning_combinations = [
+                [[0, 0], [0, 1], [0, 2]], // player took over the first row
+                [[1, 0], [1, 1], [1, 2]], // player took over the second row
+                [[2, 0], [2, 1], [2, 2]], // player took over the third row
+
+                [[0, 0], [1, 0], [2, 0]], // player took over the first column
+                [[0, 1], [1, 1], [2, 1]], // player took over the second column
+                [[0, 2], [1, 2], [2, 2]], // player took over the third column
+
+                [[0, 0], [1, 1], [2, 2]], // player took over diagonal
+                [[0, 2], [1, 1], [2, 0]], // player took over diagonal
+            ];
+
+            var checkPosition = function(boardState) {
+                // check state for winning position or no moves left.
+                var winner;
+                angular.forEach(winning_combinations, function(combination) {
+                    winner = combination.every(function(element) {
+                        return boardState.state[element[0]][element[1]].state == $rootScope.currentPlayer;
+                    });
+                    if (winner) {
+                        boardState.winner = $rootScope.currentPlayer;
+                        boardState.status = 'unavailable';
+                    }
+                });
+            };
+
+            var updateBoardStatus = function(nextBoardIndex) {
+                var nextBoard = $rootScope.gameState[nextBoardIndex];
+                $rootScope.currentPlayer = ($rootScope.currentPlayer == 1) ? 2 : 1;
+                if (!nextBoard.winner) {
+                    // a winner for board has not been defined
+                    angular.forEach($rootScope.gameState, function(board, index) {
+                        if (index == nextBoardIndex) {
+                            board.status = 'available';
+                        } else {
+                            board.status = 'unavailable';
+                        }
+                    });
+                } else {
+                    // someone has won this board or no other moves can be made
+                    angular.forEach($rootScope.gameState, function(board, index) {
+                        if (!board.winner) {
+                            board.status = 'available';
+                        } else {
+                            board.status = 'unavailable';
+                        }
+                    });
+                }
+            }
         };
         return {
             restrict: 'AE',
