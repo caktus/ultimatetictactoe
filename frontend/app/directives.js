4a2387d831d1726fee830f35c994e9c894975c1c
Victor Rocha <victor@rochapps.com>
2015-03-01 13:49:51 -0500

    Declares available and unavailable variables

--- frontend/app/directives.js
+++ frontend/app/directives.js
@@ -5,16 +5,19 @@ angular.module('TicTacToe.directives', [])
             replace: true,
             templateUrl: 'app/templates/ultimateBoard.html',
             controller: function($scope) {
-                var winning_combinations = [
-                    [0, 1, 2], // player took over the first row
-                    [3, 4, 5], // player took over the second row
-                    [6, 7, 8], // player took over the third row
-                    [0, 3, 6], // player took over the first column
-                    [1, 4, 7], // player took over the second column
-                    [2, 5, 8], // player took over the third column
-                    [0, 4, 8], // player took over diagonal
-                    [2, 4, 6]  // player took over diagonal
-                ];
+                var available = 'available',
+                    unavailable = 'unavailable',
+                    winning_combinations = [
+                        [0, 1, 2], // player took over the first row
+                        [3, 4, 5], // player took over the second row
+                        [6, 7, 8], // player took over the third row
+                        [0, 3, 6], // player took over the first column
+                        [1, 4, 7], // player took over the second column
+                        [2, 5, 8], // player took over the third column
+                        [0, 4, 8], // player took over diagonal
+                        [2, 4, 6]  // player took over diagonal
+                    ];
+
 
                 this.getCurrentPlayer = function() {
                     return $scope.game.currentPlayer;
@@ -35,18 +38,18 @@ angular.module('TicTacToe.directives', [])
                             // a winner for board has not been defined
                             angular.forEach(boards, function(board, index) {
                                 if (index == nextBoardIndex) {
-                                    board.status = 'available';
+                                    board.status = available;
                                 } else {
-                                    board.status = 'unavailable';
+                                    board.status = unavailable;
                                 }
                             });
                         } else {
                             // someone has won this board or no other moves can be made
                             angular.forEach(boards, function(board) {
                                 if (!board.winner) {
-                                    board.status = 'available';
+                                    board.status = available;
                                 } else {
-                                    board.status = 'unavailable';
+                                    board.status = unavailable;
                                 }
                             });
                         }
@@ -64,21 +67,23 @@ angular.module('TicTacToe.directives', [])
     }])
     .directive('singleBoard', [function() {
         var linker = function(scope, element, attrs, ultimateBoard) {
-            var winning_combinations = [
-                [[0, 0], [0, 1], [0, 2]], // player took over the first row
-                [[1, 0], [1, 1], [1, 2]], // player took over the second row
-                [[2, 0], [2, 1], [2, 2]], // player took over the third row
+            var available = 'available',
+                unavailable = 'unavailable',
+                winning_combinations = [
+                    [[0, 0], [0, 1], [0, 2]], // player took over the first row
+                    [[1, 0], [1, 1], [1, 2]], // player took over the second row
+                    [[2, 0], [2, 1], [2, 2]], // player took over the third row
 
-                [[0, 0], [1, 0], [2, 0]], // player took over the first column
-                [[0, 1], [1, 1], [2, 1]], // player took over the second column
-                [[0, 2], [1, 2], [2, 2]], // player took over the third column
+                    [[0, 0], [1, 0], [2, 0]], // player took over the first column
+                    [[0, 1], [1, 1], [2, 1]], // player took over the second column
+                    [[0, 2], [1, 2], [2, 2]], // player took over the third column
 
-                [[0, 0], [1, 1], [2, 2]], // player took over diagonal
-                [[0, 2], [1, 1], [2, 0]]  // player took over diagonal
-            ];
+                    [[0, 0], [1, 1], [2, 2]], // player took over diagonal
+                    [[0, 2], [1, 1], [2, 0]]  // player took over diagonal
+                ];
 
             scope.move = function(slot, rowIndex, index) {
-                if ((scope.board.status == 'available') && (slot.state == null)) {
+                if ((scope.board.status == available) && (slot.state == null)) {
                     // only squares that have not been played can be played.
                     slot.state = ultimateBoard.getCurrentPlayer();
                     updateBoardState(scope.board);
@@ -92,17 +97,17 @@ angular.module('TicTacToe.directives', [])
                     winner = isWinnerPosition(board, currentPlayer);
                 if (winner) {
                     board.winner = currentPlayer;
-                    board.status = 'unavailable';
+                    board.status = unavailable;
                 } else {
                     // check for legal moves
                     available = isAvailable(board);
                     if (available) {
-                        board.status = 'available';
+                        board.status = available;
                         return false;  // to break the loop
                     } else {
                         // otherwise is a tie
                         board.winner = 3;
-                        board.status = 'unavailable';
+                        board.status = unavailable;
                     }
                 }
             };
