0317dcd2b78ad4fcd77efd2e1feb35ce237f2c08
Victor Rocha <victor@rochapps.com>
2015-03-01 13:01:25 -0500

    Check for game winner

--- frontend/app/directives.js
+++ frontend/app/directives.js
@@ -5,6 +5,17 @@ angular.module('TicTacToe.directives', [])
             replace: true,
             templateUrl: 'app/templates/ultimateBoard.html',
             controller: function($scope) {
+                var winning_combinations = [
+                    [0, 1, 2], // player took over the first row
+                    [3, 4, 5], // player took over the second row
+                    [6, 7, 8], // player took over the third row
+                    [0, 3, 6], // player took over the first column
+                    [1, 4, 7], // player took over the second column
+                    [2, 5, 8], // player took over the third column
+                    [0, 4, 8], // player took over diagonal
+                    [2, 4, 6]  // player took over diagonal
+                ];
+
                 this.getCurrentPlayer = function() {
                     return $scope.game.currentPlayer;
                 };
@@ -12,49 +23,47 @@ angular.module('TicTacToe.directives', [])
                 this.updateGameState = function(nextBoardIndex) {
                     var boards = $scope.game.boards,
                         nextBoard = boards[nextBoardIndex],
-                        currentPlayer = $scope.game.currentPlayer;
-                    // set next player
-                    $scope.game.currentPlayer = (currentPlayer == 1) ? 2 : 1;
-                    if (!nextBoard.winner) {
-                        // a winner for board has not been defined
-                        angular.forEach(boards, function(board, index) {
-                            if (index == nextBoardIndex) {
-                                board.status = 'available';
-                            } else {
-                                board.status = 'unavailable';
-                            }
-                        });
+                        currentPlayer = $scope.game.currentPlayer,
+                        winner = isWinnerState(boards, currentPlayer);
+                    if (winner) {
+                        // game over, we have a winner!!
+                        $scope.game.winner = currentPlayer;
                     } else {
-                        // someone has won this board or no other moves can be made
-                        angular.forEach(boards, function(board) {
-                            if (!board.winner) {
-                                board.status = 'available';
-                            } else {
-                                board.status = 'unavailable';
-                            }
-                        });
+                        // continue playing
+                        $scope.game.currentPlayer = (currentPlayer == 1) ? 2 : 1;
+                        if (!nextBoard.winner) {
+                            // a winner for board has not been defined
+                            angular.forEach(boards, function(board, index) {
+                                if (index == nextBoardIndex) {
+                                    board.status = 'available';
+                                } else {
+                                    board.status = 'unavailable';
+                                }
+                            });
+                        } else {
+                            // someone has won this board or no other moves can be made
+                            angular.forEach(boards, function(board) {
+                                if (!board.winner) {
+                                    board.status = 'available';
+                                } else {
+                                    board.status = 'unavailable';
+                                }
+                            });
+                        }
                     }
-                    // check for game winner
-                    isWinnerPosition()
                 };
 
-                var isWinnerPosition = function() {
+                var isWinnerState = function(boards, currentPlayer) {
                     // checks all boards for a winning pattern
+                    return winning_combinations.some(function(combination) {
+                        return boards[combination[0]].winner == boards[combination[1]].winner == boards[combination[2]].winner == currentPlayer;
+                    });
                 }
             }
         }
     }])
     .directive('singleBoard', [function() {
         var linker = function(scope, element, attrs, ultimateBoard) {
-            scope.move = function(slot, rowIndex, index) {
-                if ((scope.board.status == 'available') && (slot.state == null)) {
-                    // only squares that have not been played can be played.
-                    slot.state = ultimateBoard.getCurrentPlayer();
-                    updateBoardState(scope.board);
-                    ultimateBoard.updateGameState((rowIndex * 3) + index);
-                }
-            };
-
             var winning_combinations = [
                 [[0, 0], [0, 1], [0, 2]], // player took over the first row
                 [[1, 0], [1, 1], [1, 2]], // player took over the second row
@@ -67,6 +76,15 @@ angular.module('TicTacToe.directives', [])
                 [[0, 0], [1, 1], [2, 2]], // player took over diagonal
                 [[0, 2], [1, 1], [2, 0]]  // player took over diagonal
             ];
+
+            scope.move = function(slot, rowIndex, index) {
+                if ((scope.board.status == 'available') && (slot.state == null)) {
+                    // only squares that have not been played can be played.
+                    slot.state = ultimateBoard.getCurrentPlayer();
+                    updateBoardState(scope.board);
+                    ultimateBoard.updateGameState((rowIndex * 3) + index);
+                }
+            };
 
             var updateBoardState = function(board) {
                 var available,
