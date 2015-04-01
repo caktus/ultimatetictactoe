4131082779a3dcff58a7a7e9bb43351e4bf20698
Jeff Bradberry <jeff.bradberry@gmail.com>
2015-04-01 14:21:51 -0400

    Removed the no longer necessary csrftoken headers from the front end.

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -7,8 +7,6 @@ var app = angular.module("TicTacToe", [
 ]);
 
 app.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
@@ -28,5 +26,5 @@ app.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpPr
         .when('/remoteMode', {
             controller: 'RemoteModeCtrl',
             templateUrl: 'app/templates/versusMode.html'
-            })
+            });
 }]);
